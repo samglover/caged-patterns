2be5cd008c27540f80f7473667aff15287a49282
samglover <sam@samglover.net>
2020-11-30 14:47:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,108 +1,42 @@
 window.addEventListener( 'resize', setFretboardHeight );
 
-let patternNav = document.querySelectorAll( '#patternNav a' );
-let fretboardContainer = document.getElementById( 'fretboard' );
-let frets = document.querySelectorAll( '.fret' );
+let patternNav          = document.querySelectorAll( '#patternNav a' );
+let fretboardContainer  = document.getElementById( 'fretboard' );
+let frets               = document.querySelectorAll( '.fret' );
 
+let intervals = {
+  major: {
+    c: [ 3, 2, 3, 2, 2 ],
+    a: [ 2, 3, 2, 2, 3 ],
+    g: [ 3, 2, 2, 3, 2 ],
+    e: [ 2, 2, 3, 2, 3 ],
+    d: [ 2, 3, 2, 3, 2 ],
+  },
+  minor: {
+    c: [ 3, 2, 2, 3, 2 ],
+    a: [ 2, 2, 3, 2, 3 ],
+    g: [ 2, 3, 2, 3, 2 ],
+    e: [ 3, 2, 3, 2, 2 ],
+    d: [ 2, 3, 2, 2, 3 ],
+  },
+};
 
-// Create empty arrays for each CAGED pattern.
-let patternC = [], patternA = [], patternG = [], patternE = [], patternD = [];
-
-frets.forEach( function( fret ) {
-  patternC.push( [ '', '', '', '', '', '' ] );
-  patternA.push( [ '', '', '', '', '', '' ] );
-  patternG.push( [ '', '', '', '', '', '' ] );
-  patternE.push( [ '', '', '', '', '', '' ] );
-  patternD.push( [ '', '', '', '', '', '' ] );
-});
-
-  // Populate the C pattern.
-  patternC[1][0] = true;
-  patternC[1][1] = true;
-  patternC[1][2] = true;
-  patternC[1][3] = true;
-  patternC[1][5] = true;
-
-  patternC[2][4] = 'root';
-
-  patternC[3][2] = true;
-  patternC[3][3] = true;
-
-  patternC[4][0] = true;
-  patternC[4][1] = 'root';
-  patternC[4][4] = true;
-  patternC[4][5] = true;
-
-
-  // Populate the A pattern
-  patternA[1][2] = true;
-  patternA[1][3] = true;
-
-  patternA[2][0] = true;
-  patternA[2][1] = 'root';
-  patternA[2][4] = true;
-  patternA[2][5] = true;
-
-  patternA[4][0] = true;
-  patternA[4][1] = true;
-  patternA[4][2] = true;
-  patternA[4][3] = 'root';
-  patternA[4][4] = true;
-  patternA[4][5] = true;
-
-
-  // Populate the G pattern.
-  patternG[1][0] = true;
-  patternG[1][1] = true;
-  patternG[1][2] = true;
-  patternG[1][3] = 'root';
-  patternG[1][4] = true;
-  patternG[1][5] = true;
-
-  patternG[3][1] = true;
-  patternG[3][2] = true;
-  patternG[3][3] = true;
-
-  patternG[4][0] = 'root';
-  patternG[4][4] = true;
-  patternG[4][5] = 'root';
-
-
-  // Populate the E pattern.
-  patternE[1][1] = true;
-  patternE[1][2] = true;
-  patternE[1][3] = true;
-
-  patternE[2][0] = 'root';
-  patternE[2][4] = true;
-  patternE[2][5] = 'root';
-
-  patternE[3][3] = true;
-
-  patternE[4][0] = true;
-  patternE[4][1] = true;
-  patternE[4][2] = 'root';
-  patternE[4][4] = true;
-  patternE[4][5] = true;
-
-
-  // Populate the D pattern.
-  patternD[0][3] = true;
-
-  patternD[1][0] = true;
-  patternD[1][1] = true;
-  patternD[1][2] = 'root';
-  patternD[1][4] = true;
-  patternD[1][5] = true;
-
-  patternD[3][0] = true;
-  patternD[3][1] = true;
-  patternD[3][2] = true;
-  patternD[3][3] = true;
-  patternD[3][5] = true;
-
-  patternD[4][4] = 'root';
-
+let roots = {
+  major: {
+    c: 4,
+    a: 3,
+    g: 2,
+    e: 1,
+    d: 5,
+  },
+  minor: {
+    c: 2,
+    a: 1,
+    g: 5,
+    e: 4,
+    d: 3,
+  },
+};
 
 function getCAGED() {
 
@@ -192,13 +126,9 @@ function drawFretboard() {
         strings[x].innerHTML = '<div class="string"></div>';
 
         if ( displayPattern[i][x] == 'root' ) {
-
           strings[x].insertAdjacentHTML( 'afterbegin', '<div class="circle-container"><div class="circle root"><span class="R">R</span></div></div>' );
-
         } else {
-
           strings[x].insertAdjacentHTML( 'afterbegin', '<div class="circle-container"><div class="circle"></div></div>' );
-
         }
 
       } else {
